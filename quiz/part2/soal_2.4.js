e1145592c2d4235dc759765dfe9a3fb61bd449a0
ShoruSenpai <shorusenpai@gmail.com>
2025-03-09 00:24:28 +0700

    Tugas kedua

---
+++ quiz/part2/soal_2.4.js
@@ -0,0 +1,31 @@
+// ## Soal 4. Breaking Sentence (yet Again) and Count Each Length
+// ```js
+// Problem
+// Mirip seperti soal nomor 3, tapi tampilkan juga panjang kata masing-masingnya!
+
+
+let word4 = 'wow JavaScript is so cool';
+
+let exampleFirstWord4 = word4.substring(0, 3);
+let firstWordLength = exampleFirstWord4.length;
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+
+let exampleSecondWord4 = word4.substring(4, 14);
+let secondWordLength = exampleSecondWord4.length;
+console.log('Second word: ' + exampleSecondWord4 + ', With length: ' + secondWordLength);
+
+let exampleThirdWord4 = word4.substring(15, 17);
+let thirdWordLength = exampleThirdWord4.length;
+console.log('Third word: ' + exampleThirdWord4 + ', With length: ' + thirdWordLength);
+
+let exampleFourthWord4 = word4.substring(18, 20);
+let fourthWordLength = exampleFourthWord4.length;
+console.log('Fourth word: ' + exampleFourthWord4 + ', With length: ' + fourthWordLength);
+
+let exampleFifthtWord4 = word4.substring(21, 25);
+let fifthWordLength = exampleFifthtWord4.length;
+console.log('Fifth word: ' + exampleFifthtWord4 + ', With length: ' + fifthWordLength);
+
+
+
+// ```
