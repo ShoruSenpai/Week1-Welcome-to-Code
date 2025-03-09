0461775817d449f42607372cec964f48cd2352b7
ShoruSenpai <shorusenpai@gmail.com>
2025-03-10 05:51:44 +0700

    tugas keempat

---
+++ quiz/part4/tugas_4.2.js
@@ -0,0 +1,23 @@
+// ## Soal 2
+// ```js
+// Buatlah sebuah fungsi bernama calculateMultiply(), yang mengembalikan nilai berupa hasil kali dari 
+// dua parameter yang dikirim.
+
+//bikin fungsinya disini
+
+function calculateMultiply() {
+    return num1 * num2;
+}
+
+let num1 = 3
+let num2 = 50
+
+let hasilPerkalian = calculateMultiply(num1,num2);
+
+// ------------------------------------
+
+console.log(`Hasil dari ${num1} * ${num2}: `)
+console.log(hasilPerkalian);
+
+// -----------------------------------
+// ```
